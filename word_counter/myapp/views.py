3e3608a3a8a4bb2708204959f7724ff1079a2191
Clement Raka De Costa <clement1raka@gmail.com>
2025-11-12 15:11:30 +0600

    made a counter html and connected it to python, django.

--- word_counter/myapp/views.py
+++ word_counter/myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 # Create your views here.
+
+def counter(request):
+    return render(request, 'counter.html')
